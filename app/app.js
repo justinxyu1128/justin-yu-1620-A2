5e75d6b9c7c3759757181bbccc8d536ad84c46fc
Justin Yu <justinxyu1128@gmail.com>
2021-07-25 23:45:56 -0700

    add display budgets function

--- app/app.js
+++ app/app.js
@@ -1,15 +1,19 @@
 const budgets = [{title: "Groceries", carrots: 50, apples: 50, total: 100}, {title: "bots", carrots: 50, apples: 50, total: 120}, {title: "cats", carrots: 50, apples: 50, total: 100}]
+
 $(".wrapper.view2").hide()
 const newbudget = document.querySelector(".button.newbudget")
 newbudget.addEventListener("click", () => {
     $(".wrapper.view1").hide()
     $(".wrapper.view2").show()
 })
+
 const allbudgets = document.querySelector(".button.allbudgets")
 allbudgets.addEventListener("click", () => {
     $(".wrapper.view2").hide()
     $(".wrapper.view1").show()
+    displayBudgets(budgets)
 })
+
 const totalbudget = document.querySelector(".totalbudget")
 totalbudget.textContent = countTotal(budgets)
 
@@ -20,4 +24,16 @@ function countTotal(array) {
     }
     const totalbudget=`Total Budget: $${total}`
     return totalbudget;
+}
+
+function displayBudgets(array) {
+    const cards = document.querySelectorAll(".card")
+    for (card of cards) {
+        card.remove()
+    }
+    for (objects of array) {
+        const budget = `<div class="card"><p class="budgettitle">${objects["title"]}</p><p class="budgetprice">$${objects["total"]}</p></div>`
+        const budgets = document.querySelector(".budgets")
+        budgets.insertAdjacentHTML("afterbegin", budget)
+    }
 }
