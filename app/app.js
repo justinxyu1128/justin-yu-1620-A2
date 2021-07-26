3f74d66f6b76d54f1a9d61b737f3dfbd4a3a991c
Justin Yu <justinxyu1128@gmail.com>
2021-07-25 22:41:16 -0700

    add change view function by pressing new budget button or all budget button

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
 const budgets = []
+$(".wrapper.view2").hide()
+const newbudget = document.querySelector(".button.newbudget")
+newbudget.addEventListener("click", () => {
+    $(".wrapper.view1").hide()
+    $(".wrapper.view2").show()
+})
+const allbudgets = document.querySelector(".button.allbudgets")
+allbudgets.addEventListener("click", () => {
+    $(".wrapper.view2").hide()
+    $(".wrapper.view1").show()
+})
